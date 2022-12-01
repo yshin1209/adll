c6c9dad1caee379ceed13c68837facf9594e696d
yshin1209 <yshin1209@gmail.com>
2022-12-01 23:26:06 +0000

    Added test functions

--- test/test_graph.py
+++ test/test_graph.py
@@ -12,7 +12,18 @@ class TestGraph (unittest.TestCase):
     """Class for testing graph.py"""
     def test_add_node(self):
         """Function for testing add_node"""
-        g.add_node ('Person', 'name', 'sThomas')
+        # To be implemented
+
+    def test_set_node_prop(self):
+        """Function for testing set_node_prop"""
+        g.add_node ('Person', 'name', 'Juno')
+        result = g.set_node_prop('name', 'Juno', 'age', 18)
+        self.assertEqual(result['age'], 18)
+
+
+    def test_get_node_prop(self):
+        """Function for testing get_node_prop"""
+        g.add_node ('Person', 'name', 'Thomas')
         self.assertEqual(g.get_node_prop('name', 'Thomas', 'name'), 'Thomas')
 
 if __name__ == '__main__':
