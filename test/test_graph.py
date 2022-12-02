584ef2f007dee06b498429aab2731170fb850af1
yshin1209 <yshin1209@gmail.com>
2022-12-02 00:53:52 +0000

    Added setUp and tearDown to test_graph.py

--- test/test_graph.py
+++ test/test_graph.py
@@ -7,23 +7,29 @@ from adll import graph
 URI = "neo4j+s://34b10577.databases.neo4j.io"
 USER = "neo4j"
 PASSWORD = "A6uVT1wvZfAFLE_O9ilqxiHuDV30in887nHF-ByS_W0"
-g = graph.Graph(graphdb, URI, USER, PASSWORD)
 class TestGraph (unittest.TestCase):
     """Class for testing graph.py"""
+
+    def setUp(self):
+        self.g = graph.Graph(graphdb, URI, USER, PASSWORD)
+
+    def tearDown(self):
+        self.g = None
+
     def test_add_node(self):
         """Function for testing add_node"""
         # To be implemented
 
     def test_set_node_prop(self):
         """Function for testing set_node_prop"""
-        g.add_node ('Person', 'name', 'Juno')
-        result = g.set_node_prop('name', 'Juno', 'age', 18)
+        self.g.add_node ('Person', 'name', 'Juno')
+        result = self.g.set_node_prop('name', 'Juno', 'age', 18)
         self.assertEqual(result['age'], 18)
 
     def test_get_node_prop(self):
         """Function for testing get_node_prop"""
-        g.add_node ('Person', 'name', 'Thomas')
-        self.assertEqual(g.get_node_prop('name', 'Thomas', 'name'), 'Thomas')
+        self.g.add_node ('Person', 'name', 'Thomas')
+        self.assertEqual(self.g.get_node_prop('name', 'Thomas', 'name'), 'Thomas')
 
 if __name__ == '__main__':
     unittest.main()
